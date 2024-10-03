6936071cd8b3b38fc7c4e22f84413332c39487fa
lucas cid <lucasfelipaaa@gmail.com>
2024-10-03 09:02:00 -0300

    feat: vercel.json adicionado para configuracoes de deploy adicionadas junto a uma nova regra para eslint

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,8 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
+      "no-unused-vars": "off",
+      "no-console": "off",
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
